8f31e7046f733fe8e72dad6a4ee6f9f5483ff85c
JacobMathew123 <padickalhouse123@gmail.com>
2025-11-15 15:50:45 +0530

    Add files via upload

---
+++ Python Programming Lab/set8_prg2.py
@@ -0,0 +1,13 @@
+import time
+import datetime
+print("Current date and time:",datetime.datetime.now())
+print("Current year:",datetime.date.today().strftime("%Y"))
+print("Month of year:",datetime.date.today().strftime("%B"))
+print("Week number of the year:",datetime.date.today().strftime("%W"))
+print("Day number of the week:",datetime.date.today().strftime("%w"))
+print("day of year:",datetime.date.today().strftime("%j"))
+print("day of the month",datetime.date.today().strftime("%d"))
+print("day of week",datetime.date.today().strftime("%A"))
+print("\nUsing time module:")
+current_time=time.localtime()
+print("Today's date:",time.strftime("%Y-%m-%d",current_time))
