780ff43182c36d463761094ed32a701485568d18
JacobMathew123 <padickalhouse123@gmail.com>
2025-11-19 02:24:04 -0800

    Add files via upload

---
+++ HTML Lab (MCA S1)/prg21.php
@@ -0,0 +1,8 @@
+<?php
+$a=array("Anjala","Christeena","Blessy","Angela");
+print_r($a);
+echo("<br>Ascending order:");assort($a);
+print_r($a);
+echo("<br>Descending order:");assort($a);
+print_r($a);
+?>
